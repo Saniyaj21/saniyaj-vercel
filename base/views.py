d3c1d9990c2fd47ac5214da94c0baae3d0277718
Saniyaj Mallik <111429200+Saniyaj21@users.noreply.github.com>
2023-01-18 11:49:59 +0530

    Add files via upload

--- base/views.py
+++ base/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Linklist
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == "POST":
+        name = request.POST['name']
+        college = request.POST['college']
+        print(name,college)
+
+        newList = Linklist.objects.create(name=name,college=college)
+
+    allList = Linklist.objects.all()
+    contaxt = {'allList':allList}
+    return render(request, 'home.html',contaxt)
